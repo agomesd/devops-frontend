eee432a9de213290d680ff340c4eecaab1bc8a1d
agomesd <alex.gduguay@gmail.com>
2024-08-16 11:07:50 +0100

    Change protocol for regions fetch

--- src/api/regions.ts
+++ src/api/regions.ts
@@ -3,7 +3,7 @@ const SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
 export async function getRegions(): Promise<string[] | null> {
   if (!SERVER_HOST) return null;
   try {
-    const res = await fetch(`http://${SERVER_HOST}/regions`);
+    const res = await fetch(`https://${SERVER_HOST}/regions`);
     return await res.json();
   } catch (error) {
     console.error("Error fetching regions: ", error);
